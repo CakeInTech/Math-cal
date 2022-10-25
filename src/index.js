64d0cbedf11b57c6f43e4cf4869ca04ac2b194d7
MohamedCK <abdulhakimm280@gmail.com>
2022-10-25 16:42:43 +0200

    `Fix Linter Errors`

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+/* eslint-disable */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+
 import App from './app';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
