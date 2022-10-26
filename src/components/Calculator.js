353cb78b61a3f9e4fd503613235a3a17dff60526
MohamedCK <abdulhakimm280@gmail.com>
2022-10-26 17:48:42 +0200

    check for linter errors and adjust styling

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -12,62 +12,62 @@ class Calculator extends React.PureComponent {
     };
   }
 
-   handleClick = (event) => {
-     const value = event.target;
+   handleClick = (events) => {
+     const { value } = events.target;
      const answer = calculate(this.state, value);
      this.setState(answer);
    }
 
-   showAnswer = (events) => {
-     this.setState({
-       total: events.target.value,
-     });
-   }
+  showAnswer = (events) => {
+    this.setState({
+      total: events.target.value,
+    });
+  }
 
-   render() {
-     const { total, next, operation } = this.state;
-     return (
-       <div className="main">
-         <div className="min-calculator">
-           <div className="screen" onChange={this.showAnswer}>
-             {total}
-             {operation}
-             {next}
-           </div>
-           <div className="button-row one">
-             <input type="button" value="AC" onClick={this.handleClick} />
-             <input type="button" value="+/-" onClick={this.handleClick} />
-             <input type="button" value="%" onClick={this.handleClick} />
-             <input type="button" value="÷" className="colored" onClick={this.handleClick} />
-           </div>
-           <div className="button-row two">
-             <input type="button" value="7" />
-             <input type="button" value="8" />
-             <input type="button" value="9" />
-             <input type="button" value="*" className="colored" onClick={this.handleClick} />
-           </div>
-           <div className="button-row three">
-             <input type="button" value="4" onClick={this.handleClick} />
-             <input type="button" value="5" onClick={this.handleClick} />
-             <input type="button" value="6" onClick={this.handleClick} />
-             <input type="button" value="-" className="colored" onClick={this.handleClick} />
-           </div>
-           <div className="button-row four">
-             <input type="button" value="1" onClick={this.handleClick} />
-             <input type="button" value="2" onClick={this.handleClick} />
-             <input type="button" value="3" onClick={this.handleClick} />
-             <input type="button" value="+" className="colored" onClick={this.handleClick} />
-           </div>
-           <div className="button-row five">
-             <input type="button" value="0" className="special" onClick={this.handleClick} />
-             <input type="button" value="." onClick={this.handleClick} />
-             <input type="button" value="=" className="colored" onClick={this.handleClick} />
-           </div>
+  render() {
+    const { total, next, operation } = this.state;
+    return (
+      <div className="main">
+        <div className="min-calculator">
+          <div className="screen" onChange={this.showAnswer}>
+            {total}
+            {operation}
+            {next}
+          </div>
+          <div className="button-row one">
+            <input type="button" value="AC" onClick={this.handleClick} />
+            <input type="button" value="+/-" onClick={this.handleClick} />
+            <input type="button" value="%" onClick={this.handleClick} />
+            <input type="button" value="÷" className="colored" onClick={this.handleClick} />
+          </div>
+          <div className="button-row two">
+            <input type="button" value="7" onClick={this.handleClick} />
+            <input type="button" value="8" onClick={this.handleClick} />
+            <input type="button" value="9" onClick={this.handleClick} />
+            <input type="button" value="x" className="colored" onClick={this.handleClick} />
+          </div>
+          <div className="button-row three">
+            <input type="button" value="4" onClick={this.handleClick} />
+            <input type="button" value="5" onClick={this.handleClick} />
+            <input type="button" value="6" onClick={this.handleClick} />
+            <input type="button" value="-" className="colored" onClick={this.handleClick} />
+          </div>
+          <div className="button-row four">
+            <input type="button" value="1" onClick={this.handleClick} />
+            <input type="button" value="2" onClick={this.handleClick} />
+            <input type="button" value="3" onClick={this.handleClick} />
+            <input type="button" value="+" className="colored" onClick={this.handleClick} />
+          </div>
+          <div className="button-row five">
+            <input type="button" value="0" className="special" onClick={this.handleClick} />
+            <input type="button" value="." onClick={this.handleClick} />
+            <input type="button" value="=" className="colored" onClick={this.handleClick} />
+          </div>
 
-         </div>
-       </div>
-     );
-   }
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Calculator;
